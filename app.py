214a5626b1e807397b69859dbee346bc62bd31dd
hrishikesh147 <hrishikeshbhagawati@gmail.com>
2023-09-17 15:16:09 +0530

    deployement ready

--- app.py
+++ app.py
@@ -49,5 +49,6 @@ def index():
 
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0",port=8080,debug=True)
+    #app.run(host="0.0.0.0",port=8080,debug=True)
+    app.run(host="0.0.0.0",port=8080)
 
